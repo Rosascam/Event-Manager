03b0db99fd45201a2a9e712e78d52d1452dd5c1a
rosasc <rosasc@D017LAB-20>
2016-12-12 13:28:18 -0500

    trying to get the create event to work

--- src/Event_Manager/Controllers/EventController.cs
+++ src/Event_Manager/Controllers/EventController.cs
@@ -19,10 +19,64 @@ namespace Event_Manager.Controllers
         }
 
 
-        public IActionResult Create()
+        public IActionResult Create(Event Ev, String addNewEvent)
+
         {
+          if (ModelState.IsValid)
+            {
+                if (addNewEvent != null)
+                {
+                    foreach (var Ev in _context.Event.ToList())
+                    {
+                        String name = artist.Name;
+                        if (name == addNewArtist)
+                        {
+                            addNewArtist = "";
+                        }
+                    }
+
+                    if (addNewArtist != "")
+                    {
+                        Artist artist = new Artist();
+                        artist.Name = addNewArtist;
+                        artist.Bio = "";
+                        _context.Artists.Add(artist);
+                        _context.SaveChanges();
+                        album.Artist = _context.Artists.Last();
+                    }
+                }
+
+                if (AddNewGenre != null)
+                {
+                    foreach (var genres in _context.Genres.ToList())
+                    {
+                        String name = genres.Name;
+                        if (name == AddNewGenre)
+                        {
+                            AddNewGenre = "";
+                        }
+                    }
+
+                    if (AddNewGenre != "")
+                    {
+                        Genre genre = new Genre();
+                        genre.Name = AddNewGenre;
+                        _context.Genres.Add(genre);
+                        _context.SaveChanges();
+                        album.Genre = _context.Genres.Last();
+                    }
+                }
+
+                _context.Albums.Add(album);
+                _context.SaveChanges();
+                return RedirectToAction("Details");
+            }
+            ViewBag.ArtistList = new SelectList(_context.Artists, "ArtistID", "Name");
+            ViewBag.GenreList = new SelectList(_context.Genres, "GenreID", "Name");
             return View();
-           
         }
+
+
     }
+}
 }
